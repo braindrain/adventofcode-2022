ad0a9282b0ed78ed491d6e58a60fe2c729ebc3ef
Gleb Uvarov <gleb.uvarov@rabobank.nl>
2022-12-15 12:45:14 +0100

    day 15 task 2

--- src/main/java/eu/uvarov/aoc22/day15/Day15.java
+++ src/main/java/eu/uvarov/aoc22/day15/Day15.java
@@ -2,15 +2,12 @@ package eu.uvarov.aoc22.day15;
 
 import eu.uvarov.aoc22.util.GenericDay;
 
+import java.math.BigInteger;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Set;
-import java.util.function.Function;
-import java.util.function.Predicate;
-import java.util.function.ToIntFunction;
 import java.util.regex.Pattern;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
+
+import static eu.uvarov.aoc22.day15.SensorBeacon.getManhattan;
 
 public class Day15 extends GenericDay {
     public Day15() {
@@ -53,12 +50,7 @@ public class Day15 extends GenericDay {
     boolean covered(int mapSize, SensorBeacon sb, int x, int y) {
         int dist = getManhattan(sb.sensor.x, sb.sensor.y, sb.beacon.x, sb.beacon.y);
         int dist2 = getManhattan(x, y, sb.sensor.x + mapSize / 2, sb.sensor.y + mapSize / 2);
-        boolean b = dist2 <= dist;
-        return b;
-    }
-
-    int getManhattan(int x1, int y1, int x2, int y2) {
-        return Math.abs(x1 - x2) + Math.abs(y1 - y2);
+        return dist2 <= dist;
     }
 
     boolean lookup(List<SensorBeacon> sbs, int x, int y, int mapSize) {
@@ -75,25 +67,35 @@ public class Day15 extends GenericDay {
 
     @Override
     public Object part2Solution() {
+        String[] strings = inputArray();
+        Pattern compile = Pattern.compile("Sensor at x=(-?\\d+), y=(-?\\d+): closest beacon is at x=(-?\\d+), y=(-?\\d+)");
+        ArrayList<SensorBeacon> sbs = new ArrayList<>();
+
+        for (String string : strings) {
+            SensorBeacon sb = regexp(string, compile, SensorBeacon.class);
+            sbs.add(sb);
+        }
+
+        for (SensorBeacon sb : sbs) {
+            //walk through each sensor's perimeter
+            for (int i = -(sb.radius + 1); i <= sb.radius; i++) {
+                int width = (sb.radius - Math.abs(i));
+
+                if (isOutsideAnySensor(sbs, (sb.sensor.x - width - 1), (sb.sensor.y + i))) {
+                    return BigInteger.valueOf(sb.sensor.x - width - 1).multiply(BigInteger.valueOf(4000000)).add(BigInteger.valueOf(((sb.sensor.y + i))));
+                }
+
+                if (isOutsideAnySensor(sbs, (sb.sensor.x + width + 1), (sb.sensor.y + i))) {
+                    return BigInteger.valueOf(sb.sensor.x + width + 1).multiply(BigInteger.valueOf(4000000)).add(BigInteger.valueOf(((sb.sensor.y + i))));
+                }
+            }
+        }
+
         return -1;
     }
 
-    boolean covered2(SensorBeacon sb, int x, int y) {
-        int dist = getManhattan(sb.sensor.x, sb.sensor.y, sb.beacon.x, sb.beacon.y);
-        int dist2 = getManhattan(x, y, sb.sensor.x, sb.sensor.y);
-        boolean b = dist2 <= dist;
-        return b;
-    }
-
-    boolean lookup2(List<SensorBeacon> sbs, int x, int y) {
-        for (SensorBeacon sensorBeacon : sbs) {
-            if (sensorBeacon.sensor.x == x && sensorBeacon.sensor.y == y) {
-                return true;
-            }
-            if (sensorBeacon.beacon.x == x && sensorBeacon.beacon.y == y) {
-                return true;
-            }
-        }
-        return false;
+    private boolean isOutsideAnySensor(List<SensorBeacon> sensors, int i, int j) {
+        return i >= 0 && i <= 4000000 && j >= 0 && j <= 4000000
+                && sensors.stream().noneMatch(sensor -> sensor.withinRadius(i, j));
     }
 }
--- src/main/java/eu/uvarov/aoc22/day15/SensorBeacon.java
+++ src/main/java/eu/uvarov/aoc22/day15/SensorBeacon.java
@@ -6,8 +6,19 @@ public class SensorBeacon {
 
     public Position beacon;
 
+    public int radius;
+
     public SensorBeacon(String sx, String sy, String bx, String by) {
         sensor = new Position(Integer.parseInt(sx), Integer.parseInt(sy));
         beacon = new Position(Integer.parseInt(bx), Integer.parseInt(by));
+        radius = getManhattan(sensor.x, sensor.y, beacon.x, beacon.y);
+    }
+
+    public static int getManhattan(int x1, int y1, int x2, int y2) {
+        return Math.abs(x1 - x2) + Math.abs(y1 - y2);
+    }
+
+    public boolean withinRadius(int x, int y) {
+        return getManhattan(x, y, sensor.x, sensor.y) <= radius;
     }
 }
